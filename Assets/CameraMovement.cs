1c9d1d70bf563fc1f27221e683d08c6cbd0205c8
Tokyo <antonioa.golpe@gmail.com>
2019-10-24 21:23:53 +0200

    Transparentar objetos que tapan al player
    -> Añadido material al objeto "Muro"
    
    -> El "Muro" se transparenta si tapa al jugador

--- Assets/CameraMovement.cs
+++ Assets/CameraMovement.cs
@@ -6,6 +6,7 @@ public class CameraMovement : MonoBehaviour
 {
     public GameObject player;
 
+    //Variable para mover la camara
     [Range(-10, 0)]
     public int offset_x;
 
@@ -15,34 +16,55 @@ public class CameraMovement : MonoBehaviour
     [Range(0, 10)]
     public int offset_z;
 
-    //[Range(-2f, 2f)]
-    //public float rotateX;
-
-    //[Range(-2f, 2f)]
-    //public float rotateY;
-
-    //[Range(-2f, 2f)]
-    //public float rotateZ;
-
-    //[Range(-2f, 2f)]
-    //public float rotateW;
-
-    //private Vector3 offset;
+    //Variables para controlar la transparencia de los objetos que impiden ver el personaje
+    //¿Haria falta que las variables fueran un array por si hay mas de un objeto tapando al personaje?
+    private Material material = null;   // Material que se quiere transparentar o opacar
+    private Color32 color;              // Color del que se quiere pintar
+    private byte alpha = 0;             // Canal alpha del material
+    private bool transparente = false;  // Esta el objeto en transparente o opaco
 
     // Start is called before the first frame update
     void Start()
     {
-        //offset = new Vector3(offset_x, offset_y, offset_z);
         transform.position = player.transform.position + new Vector3(offset_x, offset_y, offset_z);
-        //rotateX = transform.rotation.x;
-        //rotateY = transform.rotation.y;
-        //rotateZ = transform.rotation.z;
-        //rotateW = transform.rotation.w;
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.position = player.transform.position + new Vector3(offset_x, offset_y, offset_z);
+
+        RaycastHit hit;
+        //Comprobamos que no haya ningun objeto en el medio
+        //Para ello trazamos una linea desde la vista del enemigo hasta la posicion del jugador
+        if (Physics.Linecast(this.transform.position, player.transform.position, out hit))
+        {
+            //Si colisiona con el jugador eso significa que no hay obstaculos en el medio y por lo tanto lo hemos encontrado
+            if (!hit.transform.CompareTag("Player"))  //TODO: comprobar que tampoco son enemigos ni el rey
+            {
+                //Obtenemos el material del objeto que nos tapa al jugador
+                material = hit.transform.gameObject.GetComponent<MeshRenderer>().material;
+          
+                if (material != null && !transparente) //Si tiene material y aun no lo transparentamos
+                {
+                    color = material.GetColor("_Color");
+                    alpha = color.a;
+                    color.a = 20;
+                    material.SetColor("_Color", color);
+                    transparente = true;
+                }
+            }
+            else
+            {
+                if (transparente) //Cuando dejemos de verlo lo volvemos a poner a su color original
+                {
+                    color.a = alpha;
+                    material.SetColor("_Color", color);
+                    material = null;
+                    transparente = false;
+                }
+            }
+        }
+
     }
 }
